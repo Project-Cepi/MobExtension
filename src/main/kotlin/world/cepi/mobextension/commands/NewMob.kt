e3dd65d130ab892f80102ae926096c3432d606ea
CmdrNorthpaw <thetuxkid@gmail.com>
2020-11-04 14:16:41 +0000

    Some tweaks to the new mob command

--- src/main/kotlin/world/cepi/mobextension/commands/NewMob.kt
+++ src/main/kotlin/world/cepi/mobextension/commands/NewMob.kt
@@ -1,5 +1,7 @@
 package world.cepi.mobextension.commands
 
+import net.minestom.server.chat.ChatColor
+import net.minestom.server.chat.ColoredText
 import net.minestom.server.command.CommandProcessor
 import net.minestom.server.command.CommandSender
 import net.minestom.server.data.DataImpl
@@ -17,11 +19,15 @@ class NewMob: CommandProcessor {
     override fun process(sender: CommandSender, command: String, args: Array<out String>): Boolean {
         if (sender !is Player) return false
         val mobEgg = ItemStack(Material.CHICKEN_SPAWN_EGG, 1)
+        mobEgg.displayName = ColoredText.of(ChatColor.GOLD, "Mob Creation Egg")
         val data = DataImpl()
 
         data.set("id", genRandomID(), String::class.java)
         data.set("type", EntityType.CHICKEN, EntityType::class.java)
         data.set("ai", false, Boolean::class.java)
+        data.set("speed", 0F, Float::class.java)
+        data.set("health", 20F, Float::class.java)
+        data.set("name", "", String::class.java)
 
         mobEgg.data = data
         sender.inventory.addItemStack(mobEgg)
