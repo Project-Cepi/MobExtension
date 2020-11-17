daedf3d8864a88e30e2f4a4110ae98f293a49653
CmdrNorthpaw <thetuxkid@gmail.com>
2020-11-17 15:26:48 +0000

    Some preliminary work on goals

---
+++ src/main/kotlin/world/cepi/mobextension/goals/Goal.kt
@@ -0,0 +1,4 @@
+package world.cepi.mobextension.goals
+
+data class Goal(val priority: Int, val type: GoalType) {
+}
---
+++ src/main/kotlin/world/cepi/mobextension/goals/GoalType.kt
@@ -0,0 +1,5 @@
+package world.cepi.mobextension.goals
+
+enum class GoalType {
+    ATTACK_PLAYERS
+}
