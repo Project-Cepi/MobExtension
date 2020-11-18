9f795d1b0e3359bf4a6b4a49b5d36c570c1ebfad
LeoDog896 <LeoDog896@hotmail.com>
2020-11-18 13:10:34 -0500

    Actually apply the name to the entity

--- src/main/kotlin/world/cepi/mobextension/mob/meta/NameMeta.kt
+++ src/main/kotlin/world/cepi/mobextension/mob/meta/NameMeta.kt
@@ -1,12 +1,13 @@
 package world.cepi.mobextension.mob
 
+import net.minestom.server.chat.ColoredText
 import net.minestom.server.entity.Entity
 import net.minestom.server.entity.Player
 import net.minestom.server.command.builder.arguments.*
 
 class NameMeta : MobMeta<String> {
     override fun renderTask(entity: Entity, executor: Player, type: String) {
-
+        entity.customName = ColoredText.of(type)
     }
 
     override fun generateArgument(type: String): Argument<*> {
