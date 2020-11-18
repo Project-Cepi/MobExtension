fbed97eba54268dae8d8bb031d29d76a94faae98
LeoDog896 <LeoDog896@hotmail.com>
2020-11-18 11:16:18 -0500

    MobMeta functions and generics

--- src/main/kotlin/world/cepi/mobextension/mob/MobMeta.kt
+++ src/main/kotlin/world/cepi/mobextension/mob/MobMeta.kt
@@ -4,6 +4,29 @@ import net.minestom.server.command.builder.arguments.Argument
 import net.minestom.server.entity.Entity
 import net.minestom.server.entity.Player
 
-open class MobMeta {
-    fun renderTask(entity: Entity, executor: Player, arguments: List<Argument<*>>) {}
+/**
+ * Class used for handling MobMeta.
+ */
+interface MobMeta<T> {
+
+    /**
+     * This runs when the mob is created.
+     * 
+     * @param entity The entity the task is acting upon.
+     * 
+     * @param executor The player who is editing this mob
+     * 
+     * @param type The type used, whether its an item, a selector, or more.
+     * 
+     */
+    fun renderTask(entity: Entity, executor: Player, type: T)
+
+    /**
+     * Generates an Argument corresponding with the type.
+     * 
+     * @param type The type to use, from the class
+     * 
+     * @return The argument generated from that type.
+     */
+    fun generateArgument(type: T): Argument<*>
 }
