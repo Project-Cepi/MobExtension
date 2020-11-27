ceee541276c1a02e9406d509490b66a0854233a4
CmdrNorthpaw <thetuxkid@gmail.com>
2020-11-27 17:48:11 +0000

    Metadata!

--- src/main/kotlin/world/cepi/mobextension/api/Mob.kt
+++ src/main/kotlin/world/cepi/mobextension/api/Mob.kt
@@ -1,19 +1,27 @@
 package world.cepi.mobextension.api
 
+import net.minestom.server.entity.EntityType
 import world.cepi.mobextension.api.goals.Goal
 
 abstract class Mob(properties: Properties) {
     class Properties {
-        internal val goals = mutableListOf<Goal>()
+        val goals = mutableListOf<Goal>()
+        lateinit var type: EntityType
 
         fun addGoal(goal: Goal): Properties {
             goals.add(goal)
+            return this
+        }
+
+        fun setType(typeToSet: EntityType): Properties {
+            type = typeToSet
             return this
         }
     }
 
     init {
         val goals = properties.goals
+        var meta = MobMeta()
     }
 
 
---
+++ src/main/kotlin/world/cepi/mobextension/api/MobMeta.kt
@@ -0,0 +1,9 @@
+package world.cepi.mobextension.api
+
+import net.minestom.server.entity.EntityType
+
+data class MobMeta (
+        var name: String = "",
+        var health: Float = 20F,
+        var speed: Float = 10F
+)
