2f6e314db5f12ec55ea441470688b753de3e0b52
LeoDog896 <LeoDog896@hotmail.com>
2020-11-28 11:49:16 -0500

    Base ConditionalBuilder class

---
+++ src/main/kotlin/world/cepi/mobextension/mob/conditional/ConditionalBuilder.kt
@@ -0,0 +1,7 @@
+package world.cepi.mobextension.mob.conditional
+
+import net.minestom.server.event.Event
+import kotlin.reflect.KClass
+
+class ConditionalBuilder(val eventClass: KClass<out Event>) {
+}
