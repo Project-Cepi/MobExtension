dd6b4e2d8f094cbfa565811f22b885f34283fdf5
CmdrNorthpaw <thetuxkid@gmail.com>
2020-11-29 13:04:00 +0000

    Use Minestom's goal system in Goals

--- src/main/kotlin/world/cepi/mobextension/api/goals/Goal.kt
+++ src/main/kotlin/world/cepi/mobextension/api/goals/Goal.kt
@@ -1,4 +1,6 @@
 package world.cepi.mobextension.api.goals
 
-data class Goal(val priority: Int, val type: GoalType) {
+import net.minestom.server.entity.ai.GoalSelector
+
+data class Goal(val priority: Int, val type: GoalSelector) {
 }
