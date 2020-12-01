f31c1080f1e917ac87eaeaf24195250f56eae938
CmdrNorthpaw <thetuxkid@gmail.com>
2020-12-01 08:54:02 +0000

    Couple of tweaks to the serializer

--- src/main/kotlin/world/cepi/mobextension/mob/serialize/Serializer.kt
+++ src/main/kotlin/world/cepi/mobextension/mob/serialize/Serializer.kt
@@ -8,6 +8,7 @@ import java.io.FilenameFilter
 import com.worldturner.medeia.api.ValidationFailedException
 import com.worldturner.medeia.api.gson.MedeiaGsonApi
 import com.worldturner.medeia.schema.validation.SchemaValidator
+import kotlinx.serialization.json.Json
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import world.cepi.mobextension.MobExtension
@@ -20,12 +21,13 @@ fun serialize() {
     if (!mobFolder.exists()) mobFolder.mkdirs()
 
     val filter = FilenameFilter { _, name -> name.endsWith(".json") }
+    val schema = getSchema()
     mobFolder.list(filter)?.forEach {
         val file = File("./mobs/$it")
         if (!file.exists()) return@forEach
         try {
-            medeia.parseAll(medeia.createJsonReader(getSchema(), file.reader()))
-        } catch (e: ValidationFailedException) { MobExtension.logger.error("[MobExtension] $it cannot be loaded due to invalid JSON!") }
+            medeia.parseAll(medeia.createJsonReader(schema, file.reader()))
+        } catch (e: ValidationFailedException) { MobExtension.logger.error("[MobExtension] $it cannot be loaded due to invalid JSON!"); return }
     }
 }
 
