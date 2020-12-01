ea481ba6f456cdd61da02fa745f920abccfcc6fd
CmdrNorthpaw <thetuxkid@gmail.com>
2020-12-01 20:15:00 +0000

    New NullEntity because @NotNull is a thing

---
+++ src/main/kotlin/world/cepi/mobextension/mob/mobs/NullEntity.kt
@@ -0,0 +1,7 @@
+package world.cepi.mobextension.mob.mobs
+
+import net.minestom.server.entity.EntityCreature
+import net.minestom.server.entity.EntityType
+import net.minestom.server.utils.Position
+
+internal class NullEntity : EntityCreature(EntityType.ARMOR_STAND, Position(0F, 0F, 0F))
