d449e1bbb1a4fe7a365998453f51bd655999a1a8
CmdrNorthpaw <thetuxkid@gmail.com>
2020-12-02 20:35:46 +0000

    Add new SimpleMob class to represent a mob that can be serialized from JSON

---
+++ src/main/kotlin/world/cepi/mobextension/mob/SimpleMob.kt
@@ -0,0 +1,26 @@
+package world.cepi.mobextension.mob
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import net.minestom.server.entity.EntityType
+import world.cepi.mobextension.goal.serializable.SerializableGoal
+
+/**
+ * Represents a [Mob] that can be serialized from a JSON file
+ * @param id The unique ID of a mob. Is not actually a UUID but needs to identify the mob uniquely
+ * @param name The name of the mob that will be displayed above its head. Can be left blank
+ * @param goalList List of [SerializableGoal] objects to be applied to the mob. Will be applied as [net.minestom.server.entity.ai.GoalSelector] to the entity on spawn
+ * @param mobType The [EntityType] of the generated [net.minestom.server.entity.EntityCreature]
+ * */
+@Serializable
+class SimpleMob(
+    val id: String,
+    val name: String = "",
+    @SerialName("goals") val goalList: Array<SerializableGoal>, // goals would override a property in Mob()
+    @SerialName("type") val mobType: String
+) : Mob() {
+    init {
+        this.setType(EntityType.valueOf(mobType.toUpperCase()))
+        goalList.forEach { this.addGoal(it) }
+    }
+}
