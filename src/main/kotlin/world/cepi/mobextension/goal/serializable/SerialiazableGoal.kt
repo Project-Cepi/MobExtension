67a94931502e45bfa1166b9bf7bd0620bf7818bc
CmdrNorthpaw <thetuxkid@gmail.com>
2020-12-02 16:35:19 +0000

    SerializableGoal interface to wrap around GoalSelector

---
+++ src/main/kotlin/world/cepi/mobextension/goal/serializable/SerialiazableGoal.kt
@@ -0,0 +1,12 @@
+package world.cepi.mobextension.goal.serializable
+
+import net.minestom.server.entity.EntityCreature
+import net.minestom.server.entity.ai.GoalSelector
+
+interface SerialiazableGoal {
+    /**
+     *  @param creature The EntityCreature that the Goal will be associated with.
+     *  @return the [GoalSelector] that this SerializableGoal represents
+     */
+    fun toGoal(creature: EntityCreature): GoalSelector
+}
