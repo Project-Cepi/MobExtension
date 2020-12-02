1a87204f1477031b3c0df1b012942c9700cf3023
CmdrNorthpaw <thetuxkid@gmail.com>
2020-12-02 17:20:20 +0000

    Add some new goals and reorganize SerializableGoals into an object

--- src/main/kotlin/world/cepi/mobextension/goal/serializable/SerializableGoals.kt
+++ src/main/kotlin/world/cepi/mobextension/goal/serializable/SerializableGoals.kt
@@ -1,17 +1,34 @@
 package world.cepi.mobextension.goal.serializable
 
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
 import net.minestom.server.entity.EntityCreature
 import net.minestom.server.entity.ai.GoalSelector
 import net.minestom.server.entity.ai.goal.DoNothingGoal
+import net.minestom.server.entity.ai.goal.FollowTargetGoal
+import net.minestom.server.entity.ai.goal.MeleeAttackGoal
 import net.minestom.server.entity.ai.goal.RandomStrollGoal
+import net.minestom.server.utils.time.TimeUnit
+import net.minestom.server.utils.time.UpdateOption
 
-@Serializable
-class DoNothingGoal(private val time: Long, private val chance: Float) : SerialiazableGoal {
-    override fun toGoalSelector(creature: EntityCreature): GoalSelector = DoNothingGoal(creature, time, chance)
-}
+object SerializableGoals {
+    @Serializable
+    class DoNothingGoal(private val time: Long, private val chance: Float) : SerialiazableGoal {
+        override fun toGoalSelector(creature: EntityCreature): GoalSelector = DoNothingGoal(creature, time, chance)
+    }
 
-@Serializable
-class SerializableRandomStrollGoal(private val radius: Int) : SerialiazableGoal {
-    override fun toGoalSelector(creature: EntityCreature): GoalSelector = RandomStrollGoal(creature, radius)
+    @Serializable
+    class RandomStrollGoal(private val radius: Int) : SerialiazableGoal {
+        override fun toGoalSelector(creature: EntityCreature): GoalSelector = RandomStrollGoal(creature, radius)
+    }
+
+    @Serializable
+    class FollowTargetGoal(@Contextual private val pathUpdateOption: UpdateOption) : SerialiazableGoal {
+        override fun toGoalSelector(creature: EntityCreature): GoalSelector = FollowTargetGoal(creature, pathUpdateOption)
+    }
+
+    @Serializable
+    class MeleeAttackGoal(private val delay: Int, private val unit: TimeUnit) : SerialiazableGoal {
+        override fun toGoalSelector(creature: EntityCreature): GoalSelector = MeleeAttackGoal(creature, delay, unit)
+    }
 }
