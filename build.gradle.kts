43bb4da6272430473732043e2563416b1da2934d
CmdrNorthpaw <thetuxkid@gmail.com>
2020-12-02 20:41:19 +0000

    I honestly don't think we need JSON validation at this point

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,6 @@ dependencies {
     implementation("com.squareup.okhttp3", "okhttp", "4.9.0")
 
     implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.0.1")
-    implementation("com.worldturner.medeia", "medeia-validator-gson", "1.1.0")
 }
 
 tasks.withType<Test> {
