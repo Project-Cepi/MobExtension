c841c9e17ef83bb2589e045b365e82244f0b9472
CmdrNorthpaw <thetuxkid@gmail.com>
2020-12-02 16:48:10 +0000

    Consolidate SerializableGoals classes

--- src/main/kotlin/world/cepi/mobextension/goal/serializable/SerializableGoals.kt
+++ src/main/kotlin/world/cepi/mobextension/goal/serializable/SerializableGoals.kt
@@ -3,7 +3,13 @@ package world.cepi.mobextension.goal.serializable
 import kotlinx.serialization.Serializable
 import net.minestom.server.entity.EntityCreature
 import net.minestom.server.entity.ai.GoalSelector
+import net.minestom.server.entity.ai.goal.DoNothingGoal
 import net.minestom.server.entity.ai.goal.RandomStrollGoal
+
+@Serializable
+class DoNothingGoal(private val time: Long, private val chance: Float) : SerialiazableGoal {
+    override fun toGoalSelector(creature: EntityCreature): GoalSelector = DoNothingGoal(creature, time, chance)
+}
 
 @Serializable
 class SerializableRandomStrollGoal(private val radius: Int) : SerialiazableGoal {
