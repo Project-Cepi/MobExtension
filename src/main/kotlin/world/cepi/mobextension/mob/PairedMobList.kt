dd6ce0c69bbcd7907a685e2782091f0009dd284b
LeoDog896 <LeoDog896@hotmail.com>
2020-12-03 08:43:08 -0500

    More mob types

--- src/main/kotlin/world/cepi/mobextension/mob/PairedMobList.kt
+++ src/main/kotlin/world/cepi/mobextension/mob/PairedMobList.kt
@@ -1,12 +1,21 @@
 package world.cepi.mobextension.mob
 
-import net.minestom.server.entity.EntityCreature
 import net.minestom.server.entity.EntityType
-import net.minestom.server.entity.type.animal.EntityCat
-import net.minestom.server.entity.type.animal.EntityCow
-import kotlin.reflect.KClass
+import net.minestom.server.entity.type.animal.*
+import net.minestom.server.entity.type.monster.EntityBlaze
+import net.minestom.server.entity.type.monster.EntityEndermite
+import net.minestom.server.entity.type.monster.EntityGuardian
+import net.minestom.server.entity.type.monster.EntityZombie
 
-val mobTypeList = listOf<Pair<KClass<out EntityCreature>, EntityType>>(
-        EntityCow::class to EntityType.COW,
-        EntityCat::class to EntityType.CAT
+val mobTypeList = listOf(
+    EntityCow::class to EntityType.COW,
+    EntityCat::class to EntityType.CAT,
+    EntityPanda::class to EntityType.PANDA,
+    EntityLlama::class to EntityType.LLAMA,
+    EntityBee::class to EntityType.BEE,
+    EntityChicken::class to EntityType.CHICKEN,
+    EntityZombie::class to EntityType.ZOMBIE,
+    EntityBlaze::class to EntityType.BLAZE,
+    EntityGuardian::class to EntityType.GUARDIAN,
+    EntityEndermite::class to EntityType.ENDERMITE
 )
