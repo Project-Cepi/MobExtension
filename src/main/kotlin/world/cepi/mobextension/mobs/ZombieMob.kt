ffe520f0653986e7c6cdd4f77c33e8937607be54
LeoDog896 <LeoDog896@hotmail.com>
2020-12-11 23:02:57 -0500

    Small example fixes

--- src/main/kotlin/world/cepi/mobextension/mobs/ZombieMob.kt
+++ src/main/kotlin/world/cepi/mobextension/mobs/ZombieMob.kt
@@ -1,12 +1,13 @@
 package world.cepi.mobextension.mobs
 
 import net.minestom.server.entity.EntityType
-import world.cepi.mobextension.goal.SerializableGoals
 import world.cepi.mobextension.Mob
+import world.cepi.mobextension.goal.SerializableGoals
 import world.cepi.mobextension.meta.HealthMeta
 import world.cepi.mobextension.meta.NameMeta
 
-object ZombieMob: Mob(Mob.Properties()
+object ZombieMob: Mob(
+        Properties()
         .addMeta(HealthMeta(20f))
         .addMeta(NameMeta("Zombie"))
         .addGoal(SerializableGoals.RandomStrollGoal(5))
