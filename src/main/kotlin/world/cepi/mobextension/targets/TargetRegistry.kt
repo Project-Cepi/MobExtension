bb98771fbc2724823b1e331050b610a7b79dee96
LeoDog896 <LeoDog896@hotmail.com>
2021-02-21 10:20:34 -0500

    Add target registry

---
+++ src/main/kotlin/world/cepi/mobextension/targets/TargetRegistry.kt
@@ -0,0 +1,13 @@
+package world.cepi.mobextension.targets
+
+import world.cepi.mobextension.StaticObjectCollection
+import kotlin.reflect.KClass
+
+object TargetRegistry : StaticObjectCollection<KClass<out SerializableTarget>> {
+    override val objects = listOf(
+        SerializableTargets.SimplifiedClosestEntityTarget::class,
+        SerializableTargets.SimplifiedClosestLivingEntityTarget::class,
+        SerializableTargets.SimplifiedClosestPlayerTarget::class,
+        SerializableTargets.SimplifiedLastEntityDamagerTarget::class
+    )
+}
