04d2157cddcce278ccb5f930f0146e3ac953494b
LeoDog896 <LeoDog896@hotmail.com>
2021-04-04 14:28:50 -0400

    Drop properly

--- src/main/kotlin/world/cepi/mobextension/commands/subcommands/InfoSubcommand.kt
+++ src/main/kotlin/world/cepi/mobextension/commands/subcommands/InfoSubcommand.kt
@@ -13,12 +13,12 @@ import kotlin.reflect.full.memberProperties
 
 internal object InfoSubcommand : Command("info") {
 
-    private fun skimMobProperties(propertyName: String, unknownProperty: String, properties: Collection<Any>): Component {
+    private fun skimMobProperties(propertyName: String, unknownProperty: String, drop: String, properties: Collection<Any>): Component {
         return Component.text("$propertyName: ", NamedTextColor.GRAY)
             .append(Component.newline())
             .let { component ->
                 component.append(properties.map { target ->
-                    Component.text((target::class.simpleName?.dropLast(propertyName.length)) ?: unknownProperty, NamedTextColor.WHITE)
+                    Component.text((target::class.simpleName?.dropLast(drop.length)) ?: unknownProperty, NamedTextColor.WHITE)
                         .append(Component.text(" (${
                             target::class.memberProperties.joinToString { value ->
 
@@ -40,11 +40,11 @@ internal object InfoSubcommand : Command("info") {
             val mob = player.mob ?: return@addSyntax
 
             player.sendMessage(
-                skimMobProperties("Meta", "Unknown Meta", mob.properties.metas)
+                skimMobProperties("Meta", "Unknown Meta", "Meta", mob.properties.metas)
                     .append(Component.newline())
-                    .append(skimMobProperties("Goals", "Unknown Goal", mob.properties.goals))
+                    .append(skimMobProperties("Goals", "Unknown Goal", "Goal", mob.properties.goals))
                     .append(Component.newline())
-                    .append(skimMobProperties("Targets", "Unknown Target", mob.properties.targets))
+                    .append(skimMobProperties("Targets", "Unknown Target", "Target", mob.properties.targets))
                     .append(Component.newline())
                     .append(Component.text("Type: ", NamedTextColor.GRAY)
                         .append(Component.text(EntityData.findByType(mob.properties.type)?.displayName ?: "Unknown", NamedTextColor.WHITE)))
