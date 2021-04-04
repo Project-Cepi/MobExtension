5d0206715144de80848a106e03fb6975bccce3d1
LeoDog896 <LeoDog896@hotmail.com>
2021-04-04 12:45:52 -0400

    Move to functions

--- src/main/kotlin/world/cepi/mobextension/commands/subcommands/InfoSubcommand.kt
+++ src/main/kotlin/world/cepi/mobextension/commands/subcommands/InfoSubcommand.kt
@@ -5,6 +5,7 @@ import net.kyori.adventure.text.format.NamedTextColor
 import net.minestom.server.command.builder.Command
 import net.minestom.server.entity.Player
 import world.cepi.kstom.command.addSyntax
+import world.cepi.mobextension.EntityData
 import world.cepi.mobextension.mob
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.declaredMemberProperties
@@ -12,13 +13,31 @@ import kotlin.reflect.full.memberProperties
 
 internal object InfoSubcommand : Command("info") {
 
+    private fun skimMobProperties(propertyName: String, unknownProperty: String, properties: Collection<Any>): Component {
+        return Component.text("$propertyName: ", NamedTextColor.GRAY)
+            .append(Component.newline())
+            .let { component ->
+                component.append(properties.map { target ->
+                    Component.text(target::class.simpleName ?: unknownProperty, NamedTextColor.WHITE)
+                        .append(Component.text(" (${
+                            target::class.memberProperties.joinToString { value ->
+
+                                @Suppress("UNCHECKED_CAST")
+                                value as KProperty1<Any, *> // super hacky hack in order to trick java into thinking this is fine
+
+                                "${value.name}: ${value.get(target).toString()}"
+                            }
+                        })", NamedTextColor.GRAY))
+                        .append(Component.newline())
+                }.reduce { acc, textComponent -> acc.append(textComponent) })
+            }
+    }
+
     init {
         addSyntax { sender ->
             val player = sender as Player
 
             val mob = player.mob ?: return@addSyntax
-
-            // TODO DRY refactoring
 
             player.sendMessage(
                 Component.text("Meta: ", NamedTextColor.GRAY)
@@ -31,41 +50,12 @@ internal object InfoSubcommand : Command("info") {
                         }.reduce { acc, textComponent -> acc.append(textComponent) })
                     }
                     .append(Component.newline())
-                    .append(Component.text("Goals: ", NamedTextColor.GRAY)
-                        .append(Component.newline())
-                        .let { component ->
-                            component.append(mob.properties.goals.map { goal ->
-                                Component.text(goal::class.simpleName ?: "Unknown Goal", NamedTextColor.WHITE)
-                                    .append(Component.text(" (${
-                                        goal::class.memberProperties.joinToString { value ->
-
-                                            @Suppress("UNCHECKED_CAST")
-                                            value as KProperty1<Any, *> // super hacky hack in order to trick java into thinking this is fine
-
-                                            "${value.name}: ${value.get(goal).toString()}"
-                                        }
-                                    })", NamedTextColor.GRAY))
-                                    .append(Component.newline())
-                            }.reduce { acc, textComponent -> acc.append(textComponent) })
-                        })
+                    .append(skimMobProperties("Goals", "Unknown Goal", mob.properties.goals))
                     .append(Component.newline())
-                    .append(Component.text("Targets: ", NamedTextColor.GRAY)
-                        .append(Component.newline())
-                        .let { component ->
-                            component.append(mob.properties.targets.map { target ->
-                                Component.text(target::class.simpleName ?: "Unknown Target", NamedTextColor.WHITE)
-                                    .append(Component.text(" (${
-                                        target::class.memberProperties.joinToString { value ->
-
-                                            @Suppress("UNCHECKED_CAST")
-                                            value as KProperty1<Any, *> // super hacky hack in order to trick java into thinking this is fine
-
-                                            "${value.name}: ${value.get(target).toString()}"
-                                        }
-                                    })", NamedTextColor.GRAY))
-                                    .append(Component.newline())
-                            }.reduce { acc, textComponent -> acc.append(textComponent) })
-                        })
+                    .append(skimMobProperties("Targets", "Unknown Target", mob.properties.targets))
+                    .append(Component.newline())
+                    .append(Component.text("Type: ", NamedTextColor.GRAY)
+                        .append(Component.text(EntityData.findByType(mob.properties.type)?.displayName ?: "Unknown", NamedTextColor.WHITE)))
             )
 
         }
