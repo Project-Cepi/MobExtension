ae47670d29277009122d0c31e228c032872c5916
LeoDog896 <LeoDog896@hotmail.com>
2021-05-17 11:07:48 -0400

    Add cat color meta

--- src/main/kotlin/world/cepi/mobextension/meta/MetaObjectCollection.kt
+++ src/main/kotlin/world/cepi/mobextension/meta/MetaObjectCollection.kt
@@ -4,23 +4,5 @@ import world.cepi.mobextension.StaticObjectCollection
 import kotlin.reflect.KClass
 
 object MetaObjectCollection: StaticObjectCollection<KClass<out MobMeta>> {
-    override val objects = listOf(
-            HealthMeta::class,
-            NameMeta::class,
-            GlowingMeta::class,
-            FireMeta::class,
-            SilentMeta::class,
-            GravityMeta::class,
-            InvisibleMeta::class,
-            ExperienceMeta::class,
-            BabyMeta::class,
-
-            LlamaColorMeta::class,
-
-            SheepShearedMeta::class,
-
-            ArmorStandSmallMeta::class,
-            ArmorStandBasePlateMeta::class,
-            ArmorStandArmMeta::class
-    )
+    override val objects = MobMeta::class.sealedSubclasses
 }
