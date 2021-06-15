672d83ec05c24e2d306aea5d93a27372d69af093
LeoDog896 <LeoDog896@hotmail.com>
2021-06-15 15:34:45 -0400

    Bump KStom

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     compileOnly("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.2.1")
 
     // implement KStom
-    compileOnly("com.github.Project-Cepi:KStom:3453ca673b")
+    compileOnly("com.github.Project-Cepi:KStom:5a2a3a30bd")
 
     // Use mworlza's canvas
     implementation("com.mattworzala:canvas:1.1.3")
