7d218799193ecdad4e597bae14d4eab181d9703e
LeoDog896 <LeoDog896@hotmail.com>
2021-09-26 19:59:39 -0400

    Add pagination

--- src/main/kotlin/world/cepi/mob/ui/TypeScreen.kt
+++ src/main/kotlin/world/cepi/mob/ui/TypeScreen.kt
@@ -1,11 +1,15 @@
 package world.cepi.mob.ui
 
 import com.mattworzala.canvas.Slot
+import com.mattworzala.canvas.extra.row
 import com.mattworzala.canvas.fragment
+import com.mattworzala.canvas.useState
 import net.kyori.adventure.sound.Sound
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextDecoration
+import net.minestom.server.item.ItemStack
+import net.minestom.server.item.Material
 import net.minestom.server.sound.SoundEvent
 import world.cepi.kstom.util.component1
 import world.cepi.kstom.util.component2
@@ -13,11 +17,11 @@ import world.cepi.kstom.util.component3
 import world.cepi.mob.mob.EntityEggData
 import world.cepi.mob.mob.mobEgg
 
+private const val PAGE_SIZE = 9 * 4
+
 fun TypeScreen() = fragment(9, 6) {
 
-    this.container.title = Component.text("Choose a Type")
-
-    EntityEggData.values().map {
+    val menuItems = EntityEggData.values().map {
         { slot: Slot ->
             slot.item(it.material) {
                 displayName(
@@ -41,11 +45,41 @@ fun TypeScreen() = fragment(9, 6) {
                 event.player.itemInMainHand = mob.generateEgg(event.player.itemInMainHand)
             }
         }
-    }.forEachIndexed { index, slot ->
+    }
 
-        if (index > (9 * 6) - 1) return@forEachIndexed
+    var page by useState(0);
 
-        slot(this[index])
+    val items = menuItems
+        .drop(page * PAGE_SIZE)
+        .take(PAGE_SIZE)
+
+    this.container.title = Component.text("Choose a Type")
+
+    row(0, 5) {
+        item = ItemStack.of(Material.WHITE_STAINED_GLASS_PANE).withDisplayName(Component.empty())
+    }
+
+    slot(0, 5) {
+        item = ItemStack.of(Material.RED_STAINED_GLASS_PANE)
+            .withDisplayName(Component.text("Previous Page", NamedTextColor.RED))
+
+        onClick {
+            page = (page - 1).coerceAtLeast(0)
+        }
+    }
+
+    slot(8, 5) {
+        item = ItemStack.of(Material.LIME_STAINED_GLASS_PANE)
+            .withDisplayName(Component.text("Next Page", NamedTextColor.GREEN))
+
+        onClick {
+            page = (page + 1).coerceAtLeast(0)
+        }
+    }
+
+    items.forEachIndexed { index, slot ->
+
+        slot(this[index + 9])
     }
 
 
