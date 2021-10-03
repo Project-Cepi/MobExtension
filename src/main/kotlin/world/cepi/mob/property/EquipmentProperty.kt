a8b26362c0d09b003536c23598a42bc8dccf5713
LeoDog896 <LeoDog896@hotmail.com>
2021-10-03 09:30:18 -0400

    Update EquipmentProperty.kt

--- src/main/kotlin/world/cepi/mob/property/EquipmentProperty.kt
+++ src/main/kotlin/world/cepi/mob/property/EquipmentProperty.kt
@@ -2,82 +2,81 @@ package world.cepi.mob.property
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import net.minestom.server.entity.Entity
+import net.minestom.server.entity.EntityCreature
 import net.minestom.server.entity.EquipmentSlot
 import net.minestom.server.inventory.EquipmentHandler
 import net.minestom.server.item.ItemStack
 import world.cepi.kstom.command.arguments.context.OffItemContextParser
 import world.cepi.kstom.command.arguments.generation.annotations.ParameterContext
 import world.cepi.kstom.serializer.ItemStackSerializer
-import world.cepi.mob.meta.MobMeta
 
 @Serializable
 @SerialName("helmet")
-data class HelmetMeta(
+data class HelmetProperty(
     @param:ParameterContext(OffItemContextParser::class)
     @Serializable(with = ItemStackSerializer::class)
     val itemStack: ItemStack
-) : MobMeta() {
-    override fun apply(entity: Entity) {
-        (entity as? EquipmentHandler ?: return).setEquipment(EquipmentSlot.HELMET, itemStack)
+) : MobProperty() {
+    override fun apply(creature: EntityCreature) {
+        (creature as? EquipmentHandler ?: return).setEquipment(EquipmentSlot.HELMET, itemStack)
     }
 }
 
 @Serializable
 @SerialName("chestplate")
-data class ChestplateMeta(
+data class ChestplateProperty(
     @param:ParameterContext(OffItemContextParser::class)
     @Serializable(with = ItemStackSerializer::class)
     val itemStack: ItemStack
-) : MobMeta() {
-    override fun apply(entity: Entity) {
-        (entity as? EquipmentHandler ?: return).setEquipment(EquipmentSlot.CHESTPLATE, itemStack)
+) : MobProperty() {
+    override fun apply(creature: EntityCreature) {
+        (creature as? EquipmentHandler ?: return).setEquipment(EquipmentSlot.CHESTPLATE, itemStack)
     }
 }
 
 @SerialName("leggings")
-data class LeggingsMeta(
+data class LeggingsProperty(
     @param:ParameterContext(OffItemContextParser::class)
     @Serializable(with = ItemStackSerializer::class)
     val itemStack: ItemStack
-) : MobMeta() {
-    override fun apply(entity: Entity) {
-        (entity as? EquipmentHandler ?: return).setEquipment(EquipmentSlot.LEGGINGS, itemStack)
+) : MobProperty() {
+    override fun apply(creature: EntityCreature) {
+        (creature as? EquipmentHandler ?: return).setEquipment(EquipmentSlot.LEGGINGS, itemStack)
     }
 }
 
 @Serializable
 @SerialName("boots")
-data class BootsMeta(
+data class BootsProperty(
     @param:ParameterContext(OffItemContextParser::class)
     @Serializable(with = ItemStackSerializer::class)
     val itemStack: ItemStack
-) : MobMeta() {
-    override fun apply(entity: Entity) {
-        (entity as? EquipmentHandler ?: return).setEquipment(EquipmentSlot.BOOTS, itemStack)
+) : MobProperty() {
+    override fun apply(creature: EntityCreature) {
+        (creature as? EquipmentHandler ?: return).setEquipment(EquipmentSlot.BOOTS, itemStack)
     }
 }
 
 @Serializable
 @SerialName("off_hand")
-data class OffHandMeta(
+data class OffHandProperty(
     @param:ParameterContext(OffItemContextParser::class)
     @Serializable(with = ItemStackSerializer::class)
     val itemStack: ItemStack
-) : MobMeta() {
-    override fun apply(entity: Entity) {
-        (entity as? EquipmentHandler ?: return).setEquipment(EquipmentSlot.OFF_HAND, itemStack)
+) : MobProperty() {
+    override fun apply(creature: EntityCreature) {
+        (creature as? EquipmentHandler ?: return).setEquipment(EquipmentSlot.OFF_HAND, itemStack)
     }
 }
 
 @Serializable
 @SerialName("main_hand")
-data class MainHandMeta(
+data class MainHandProperty(
     @param:ParameterContext(OffItemContextParser::class)
     @Serializable(with = ItemStackSerializer::class)
     val itemStack: ItemStack
-) : MobMeta() {
-    override fun apply(entity: Entity) {
-        (entity as? EquipmentHandler ?: return).setEquipment(EquipmentSlot.MAIN_HAND, itemStack)
+) : MobProperty() {
+    override fun apply(creature: EntityCreature) {
+        (creature as? EquipmentHandler ?: return).setEquipment(EquipmentSlot.MAIN_HAND, itemStack)
     }
 }
