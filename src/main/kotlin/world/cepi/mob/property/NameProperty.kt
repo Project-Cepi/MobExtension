f60342e27884861d62feb3d022cd07b91a58b471
LeoDog896 <LeoDog896@hotmail.com>
2021-12-15 19:32:32 -0500

    Make name always visible

--- src/main/kotlin/world/cepi/mob/property/NameProperty.kt
+++ src/main/kotlin/world/cepi/mob/property/NameProperty.kt
@@ -6,16 +6,25 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.minestom.server.entity.EntityCreature
 import world.cepi.kstom.adventure.asMini
+import world.cepi.kstom.command.arguments.generation.annotations.DefaultBoolean
 import world.cepi.kstom.command.arguments.generation.annotations.DefaultNumber
 
 @Serializable
 @SerialName("name")
-data class NameProperty(@SerialName("value") val name: String, @DefaultNumber(1.0) val level: Int) : MobProperty() {
+data class NameProperty(
+    @SerialName("value")
+    val name: String,
+    @DefaultNumber(1.0)
+    val level: Int = 1,
+    @DefaultBoolean(true)
+    val alwaysVisible: Boolean = true
+) : MobProperty() {
     override fun apply(entityCreature: EntityCreature) {
         entityCreature.customName = (if (level > 0) Component.text("[", NamedTextColor.DARK_GRAY)
             .append(Component.text(level, NamedTextColor.GRAY))
             .append(Component.text("] ", NamedTextColor.DARK_GRAY)) else Component.space())
             .append(name.asMini().color(NamedTextColor.GRAY))
+        entityCreature.isCustomNameVisible = alwaysVisible
     }
 
 }
