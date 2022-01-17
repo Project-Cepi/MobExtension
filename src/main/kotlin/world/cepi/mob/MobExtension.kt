d30df4a0c3a45def3ed13ee1c314930a84c5472e
LeoDog896 <LeoDog896@gmail.com>
2022-01-17 11:13:05 -0500

    Remove unnecessary imports

--- src/main/kotlin/world/cepi/mob/MobExtension.kt
+++ src/main/kotlin/world/cepi/mob/MobExtension.kt
@@ -1,16 +1,8 @@
 package world.cepi.mob
 
-import net.minestom.server.entity.EntityCreature
-import net.minestom.server.entity.EntityType
-import net.minestom.server.entity.ai.EntityAIGroup
-import net.minestom.server.entity.ai.goal.MeleeAttackGoal
-import net.minestom.server.entity.ai.target.ClosestEntityTarget
 import net.minestom.server.event.EventFilter
 import net.minestom.server.event.EventNode
-import net.minestom.server.event.player.PlayerSpawnEvent
 import net.minestom.server.extensions.Extension
-import net.minestom.server.utils.time.TimeUnit
-import world.cepi.kstom.Manager
 import world.cepi.kstom.event.listenOnly
 import world.cepi.kstom.util.log
 import world.cepi.kstom.util.node
@@ -18,7 +10,6 @@ import world.cepi.mob.commands.MobCommand
 import world.cepi.mob.events.MobInteractHook
 import world.cepi.mob.events.MobSpawnHook
 import world.cepi.mob.events.MobUIHook
-import world.cepi.mob.mob.Mob
 import world.cepi.mob.spawner.MobSpawner
 import java.io.File
 
