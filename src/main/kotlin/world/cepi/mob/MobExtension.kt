e9cba245935e69a11ee99caeac90a34e20a6dbdd
Tristan <LeoDog896@hotmail.com>
2022-04-03 10:19:26 -0400

    Save mob spawners

--- src/main/kotlin/world/cepi/mob/MobExtension.kt
+++ src/main/kotlin/world/cepi/mob/MobExtension.kt
@@ -1,8 +1,13 @@
 package world.cepi.mob
 
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.serializer
 import net.minestom.server.event.EventFilter
 import net.minestom.server.event.EventNode
 import net.minestom.server.extensions.Extension
+import world.cepi.actions.ActionSerializer
 import world.cepi.kstom.event.listenOnly
 import world.cepi.kstom.util.log
 import world.cepi.kstom.util.node
@@ -13,9 +18,21 @@ import world.cepi.mob.events.MobUIHook
 import world.cepi.mob.mob.Mob
 import world.cepi.mob.spawner.MobSpawner
 import java.io.File
+import kotlin.io.path.*
 
 class MobExtension : Extension() {
+
+    val format = Json { serializersModule = ActionSerializer.module }
+    private val spawnersFile by lazy { dataDirectory().resolve("mob-spawners.json") }
+
     override fun initialize(): LoadStatus {
+
+        if (spawnersFile.exists())
+            MobSpawner.mutableSpawners = format.decodeFromString(spawnersFile.readText())
+        else {
+            spawnersFile.createDirectories()
+            spawnersFile.createFile()
+        }
 
         val playerNode = EventNode.type("general-mob-hooks", EventFilter.PLAYER)
 
@@ -39,6 +56,8 @@ class MobExtension : Extension() {
     }
 
     override fun terminate() {
+
+        spawnersFile.writeText(format.encodeToString(MobSpawner.mutableSpawners))
 
         MobCommand.unregister()
 
--- src/main/kotlin/world/cepi/mob/spawner/MobSpawner.kt
+++ src/main/kotlin/world/cepi/mob/spawner/MobSpawner.kt
@@ -1,5 +1,7 @@
 package world.cepi.mob.spawner
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import net.minestom.server.MinecraftServer
 import net.minestom.server.coordinate.Pos
 import net.minestom.server.entity.Entity
@@ -10,31 +12,38 @@ import net.minestom.server.instance.Instance
 import net.minestom.server.timer.Task
 import net.minestom.server.utils.time.TimeUnit
 import world.cepi.kstom.event.listenOnly
+import world.cepi.kstom.serializer.DurationSerializer
+import world.cepi.kstom.serializer.PositionSerializer
 import world.cepi.mob.mob.Mob
+import java.io.Serial
 import java.time.Duration
 import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
 /** Represents a mob spawning, containing where to spawn, and the mob to spawn + the time/limit. */
+@Serializable
 class MobSpawner(
     /** The ID of the instance */
     val id: String,
     /** What instance this mob spawner should be located in. */
-    val instance: Instance,
+    @Transient
+    val instance: Instance = MinecraftServer.getInstanceManager().instances.first(),
     /** All viable locations this mob can spawn in. Should be one block above the ground */
-    val viablePositions: MutableList<Pos>,
+    val viablePositions: MutableList<@Serializable(with = PositionSerializer::class) Pos>,
     /** How close the nearest player should be for mobs to spawn. */
     val playerRadiusRequired: Double = 50.0,
     /** The mob to spawn */
     val mob: Mob,
     /** How many ticks it should take for the next mob to spawn. */
-    spawnOption: Duration = Duration.of(10L, TimeUnit.SECOND),
+    @Serializable(with = DurationSerializer::class)
+    private val _spawnOption: Duration = Duration.of(10L, TimeUnit.SECOND),
     /** How many mobs can be controlled by this spawner at once. */
     var limit: Int = 100
 ) {
 
     /** How much time it should take for the next mob to spawn. */
-    var spawnOption: Duration = spawnOption
+    @Transient
+    var spawnOption: Duration = _spawnOption
         set(amount) {
             field = amount
             update()
@@ -44,8 +53,10 @@ class MobSpawner(
     private var schedule: Task? = null
 
     /** The current amount of mobs linked to this spawner. Used to accurately calculate the [limit] of this spawner. */
+    @Transient
     private val amount: AtomicInteger = AtomicInteger()
 
+    @Transient
     private val entities: MutableList<Entity> = mutableListOf()
     private val node = EventNode.type("mob-spawner-${UUID.randomUUID()}", EventFilter.ENTITY) { _, obj ->
         entities.contains(obj)
@@ -92,7 +103,7 @@ class MobSpawner(
 
     companion object {
 
-        private val mutableSpawners: MutableMap<String, MobSpawner> = mutableMapOf()
+        internal var mutableSpawners: MutableMap<String, MobSpawner> = mutableMapOf()
         val spawners: Map<String, MobSpawner> = Collections.unmodifiableMap(mutableSpawners)
 
         /**
