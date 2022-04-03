31b27f4eef4ceefcee200bdcf3d9c387f20af7b4
Tristan <LeoDog896@hotmail.com>
2022-04-03 15:11:08 -0400

    Bump versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,10 +58,10 @@ allprojects {
         implementation("com.github.Project-Cepi:canvas:d3d937c54d")
 
         // Use the JUpiter test library.
-        testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+        testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 
         // Add Particable
-        compileOnly("com.github.Project-Cepi:Particable:e864eb601b")
+        compileOnly("com.github.Project-Cepi:Particable:acea414be2")
 
         // Add levels
         compileOnly("com.github.Project-Cepi:LevelExtension:cfcbcd8bf7")
